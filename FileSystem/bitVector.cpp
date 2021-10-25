751d6a435f8c5b4f02ef50fafd12b79edca6d0c1
Ognjen Subaric <61935601+ogsub@users.noreply.github.com>
2021-10-26 01:02:09 +0200

    Update bitVector.cpp

--- FileSystem/bitVector.cpp
+++ FileSystem/bitVector.cpp
@@ -16,22 +16,16 @@ BitVector::~BitVector()
 }
 
 void BitVector::useBitVector(int clusterNo, Partition* partition) { //setBitVectorUsed
-	//std::unique_lock<std::mutex> lock(m);
-	//myWait(mutex);
 	bitVector[clusterNo] = USED;
-	//ooopartition->writeCluster(0, (char*) this);
-	//mySignal(mutex);
 }
 
 void BitVector::freeBitVector(int clusterNo) {
-	//std::unique_lock<std::mutex> lock(m);
 	myWait(mutex);
 	bitVector[clusterNo] = FREE;
 	mySignal(mutex);
 }
 
 int BitVector::getFreeClusterNo() {
-	//std::unique_lock<std::mutex> lock(m);
 	myWait(mutex);
 	int ret = bitVector.size() - bitVector.count();
 	mySignal(mutex);
@@ -39,7 +33,6 @@ int BitVector::getFreeClusterNo() {
 }
 
 int BitVector::getUsedClusterNo() {
-	//std::unique_lock<std::mutex> lock(m);
 	myWait(mutex);
 	int ret = bitVector.count();
 	mySignal(mutex);
@@ -48,22 +41,15 @@ int BitVector::getUsedClusterNo() {
 
 int BitVector::getFirstFreeClusterNo(Partition* partition) {
 	std::unique_lock<std::mutex> lock(m);
-	//myWait(mutex);
-	//std::unique_lock<std::mutex> lock(m);
 	for (int i = 0; i < bitVector.size(); i++) {
 		if (bitVector[i] == FREE) { 
-			//mySignal(mutex);
-			///////////////////////////////////
 			bitVector[i] = USED;
-			//@@@@@@@@@@@@@@@@@partition->writeCluster(0, (char*)this);
-			///////////////////////////////////
 			return i; 
 		}
 	}
 }
 
 bool BitVector::isFree(int clusterNo) {
-	//std::unique_lock<std::mutex> lock(m);
 	myWait(mutex);
 	if (bitVector[clusterNo] == FREE) {
 		mySignal(mutex);
